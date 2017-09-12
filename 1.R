a528cfe4f01ca2dd3139bbc8157bf64f18a60879
aporvakarn <karnapoorva@gmail.com>
2017-09-12 20:37:30 +0530

    ac

--- 1.R
+++ 1.R
@@ -15,3 +15,132 @@ savehistory()
 save.image()
 
 library()
+
+.libPaths()
+library()
+library(gclus)
+
+HELP()
+Help()
+help()
+ 
+vector
+vector()
+vector(1)
+vector(1:100)
+
+vec1=c(1,4,6,8,10)
+vec1
+vec2=c(1:100)
+vec2
+vec3=c(vec1,0,vec2) 
+vec3
+vec1(5)
+vec1[5]
+vec1[3]=12
+vec1
+vec2=seq(from=0,to=1, by=0.25)
+vec2
+vec1+2
+length(vec1)
+length(vec1+2)
+vec1[vec1>2]
+vector("numeric",5)
+vector("complex",5)
+>u=c(10,20,30)
+u=c(10,20,30)
+v=c(1,2,3,4,5,6,7,8,9)
+u+v
+s[-3]
+s=c("aa","bb","cc","dd","ee")
+L=c(FALSE,TRUE,FALSE,TRUE,FALSE)
+s[L]
+vec1[c(FALSE,TRUE,FALSE,TRUE,FALSE)]
+v=c("Mary","Sue")
+v
+names(v)=c("First","Last")
+v
+v[c("Last","First")]
+
+x=c("first"=3,"second"=0,"third"=9)
+length(x)
+x=apoorva
+x="apoorva"
+substr(x,1,5)
+substr(x,1,7)
+x= null
+x="null"
+vec2=null
+vec2="null"
+vec2
+
+sort(x)
+order(x)
+x=c(1,2,3,4,5,6,7)
+x[order(x)]
+sort(x)
+x1=c(1,4,7,2,3)
+x1[order(x1)]
+sort(x)
+order(x1)
+sort(x1)
+order(x1,decreasing = TRUE)
+v=c(1:100)
+
+x=1:3
+y=rep(x,3)
+y
+rep(x,c(1,2,3))
+rep(x,c(3,1,2))
+v=paste(1:3,4:5,sep = "&")
+
+x=matrix(1:12,nrow = 4,byrow = T)
+x
+x[2,2]
+x[4,2]
+x[r,c(1,2)]
+x[ ,c(1,2)]
+x[c(1,4),2]
+dim(x)
+mymatrix[-2,-3]
+x[-2,-3]
+colnames(x)=c("sub1","sub2","sub3")
+x
+rownames(x)=c("r1","r2","r3")
+x
+
+x=c(20,30,40,50)
+cbind(x,x)
+x=matrix(1:12,nrow = 4,byrow = T)
+z=c(x)
+z
+
+x=matrix(1:12,nrow = 4,byrow = T)
+x
+colnames(x)=c("c1","c2",'c3')
+x
+rownames(x)=c("r1","r2","r3","r4")
+x
+z=c(x)
+x
+z
+x[x[,2]>5]
+x[x[2,]>3]
+patientID <- c(1, 2, 3, 4)
+age <- c(25, 34, 28, 52)
+diabetes <- c("Type1", "Type2", "Type1", "Type1")
+status <- c("Poor", "Improved", "Excellent", "Poor")
+patientdata <- data.frame(patientID, age, diabetes, status)
+patientdata
+str(patientdata)
+?str
+str
+help("str")
+str(patientID)
+patientdata[3,2]
+subset_patientdata <-patientdata[1:2,]
+subset_patientdata
+patientdata[patientdata$diabetes %in% c("Type1","Type2"),]
+patientdata[patientdata$status %in% c("Poor","Excellent"),]
+patientdata[patientdata$age>25,]
+patientdata[patientdata$age<30]
--- factors.R
+++ factors.R
@@ -4,3 +4,25 @@ x1=factor("Male","Female","Male")
 x1
 z=factor(c("low","medium","high"),ordered=T,levels = c("high","medium","low"))
 z
+gender=c(1,2,1,1,2)
+fgender=factor(gender)
+fgender
+fgender=factor(gender,levels = c(1,2), labels = c("M","F"))
+fgender
+classposition=c(1,2,3,1)
+class(classposition)
+summary(classposition)
+fclassposition=factor(classposition)
+fclassposition
+summary(fclassposition)
+levels(fclassposition)
+fclassposition2=factor(classposition,levels=c(5,4,3,2,1),ordered = T,labels = c("fifth","fourth","third","second","first"))
+fclassposition2
+summary(fclassposition2)
+fclassposition3=factor(fclassposition2,levels(fclassposition2)[c(5,3,4,2,1)])
+fclassposition3
+print(levels(fclassposition3))
+rev(levels(fclassposition2))
+fclassposition2
+fclassposition4=factor(fclassposition2,(levels = rev(fclassposition2)))
+plot(summary(fclassposition3))
