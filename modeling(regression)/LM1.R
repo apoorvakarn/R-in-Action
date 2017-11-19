5241e4cdfcdbb5a213de3390a7364b56406fdd6b
apoorvakarn <karnapoorva@gmail.com>
2017-11-19 13:46:10 +0530

    l_m

--- modeling(regression)/LM1.R
+++ modeling(regression)/LM1.R
@@ -6,11 +6,29 @@ data(women)
 #weight numeric weight (lbs)
 # linear regression y=mx+c
 attach(women)
+View(women)
 cov(height,weight)
 cor(height,weight)
-weight.lm= lm(weight~height,data = women)
+
+write.csv(women,"")
+
+fit= lm(weight~height,data = women)
+layout(matrix(c(1,2,3,4),2,2)) # optional 4 graphs/page 
+plot(fit)
+fit
+summary(fit)
+
+coefficients(fit) # model coefficients
+confint(fit, level=0.95) # CIs for model parameters 
+fitted(fit) # predicted values
+residuals(fit) # residuals
+anova(fit) # anova table 
+vcov(fit) # covariance matrix for model parameters 
+
+influence(fit) # regression 
+
 (weight.lm =lm(weight~height,data = women)) # to print the values in one line 
-summary(weight.lm)
+
 plot(height,weight)
 abline(weight.lm,col="red") #line of best fit
 abline(v=c(62,64,66),col="blue") #to draw vertical line
